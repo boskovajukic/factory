9c04b49aa1ff40728f71198be42e27a07cdf1e2e
Boško Vajukić <boskovajukic@gmail.com>
2020-02-11 20:17:45 +0100

    Vezbanka

--- src/main/java/main/factory/Car.java
+++ src/main/java/main/factory/Car.java
@@ -3,12 +3,115 @@ package main.factory;
 
 
 public class Car {
+    private final String vehicleType = "car";
+    private String model;
+    private int buildYear;
+    private int maxSpeed;
+    private String color;
+    private int maxFuel;
+    private int currentFuel;
+    private int consumption;
     
-    String model;
-    int buildYear;
-    int maxSpeed;
-    String color;
-    int maxFuel;
-    int currentFuel;
-    int consumption;
+    
+    
+    public Car() {
+        this.model = "";
+        this.buildYear = 0;
+        this.color = "";
+        this.maxFuel = 0;
+        this.currentFuel = 0;
+        this.consumption = 0;
+    }
+    
+    public  Car(String customModel, int customBuildYear, int customMaxSpeed, String customColor, int customMaxFuel, int customCurrentFuel, int customconsumption) {
+        this.model = customModel;
+        this.buildYear = customBuildYear;
+        this.maxSpeed = customMaxSpeed;
+        this.color = customColor;
+        this.maxFuel = customMaxFuel;
+        this.currentFuel = customCurrentFuel;
+        this.consumption = customconsumption;
+    }
+    
+    public String getVehicleType() {
+        return this.vehicleType;
+    }
+    
+    public String getModel() {
+        return this.model;
+    }
+    
+    public void setModel(String customModel) {
+        this.model = customModel;
+    }
+    
+    public int getBuildYear() {
+        return this.buildYear;
+    }
+    
+    public void setBuildYear(int customBuildYear) {
+                this.buildYear = customBuildYear;
+    }
+    
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+    
+    public void setMaxSpeed(int customMaxSpeed) {
+                this.maxSpeed = customMaxSpeed;
+    }
+    
+    public String getColor() {
+        return this.color;
+    }
+    
+    public void setColor(String customColor) {
+        this.color = customColor;
+    }
+    
+    public int getMaxFuel() {
+        return this.maxFuel;
+    }
+    
+    public void setMaxFuel(int customMaxFuel) {
+                this.maxFuel = customMaxFuel;    
+    }
+    
+    public int getCurrentFuel() {
+        return this.currentFuel;
+    }
+    
+    public void setCurrentFuel(int customCurrentFuel) {
+                this.currentFuel = customCurrentFuel;
+    }
+    
+    public int getConsumption() {
+        return this.consumption;
+    }
+    
+    public void setconsumption(int customconsumption) {
+                this.consumption = customconsumption;
+    }
+    
+    public void showData() {
+        System.out.println("Model: " + this.getModel());
+        System.out.println("Godina proizvodnje: " + this.getBuildYear());
+        System.out.println("Boja: " + this.getColor());
+        System.out.println("Potrošnja: " + this.getConsumption());
+        System.out.println("Stanje rezervoara: " + this.getCurrentFuel());
+        System.out.println("Kapacitet rezervoara je: " + this.getMaxFuel());
+        System.out.println("Maksimalna brzina: " + this.getMaxSpeed());
+        
+    }
+    
+    public void travel(int distanceTraveled) {
+        // kolicina goriva da se smanji za onoliko koliko je potroseno
+        // kilometraza treba da se uveca
+        //kuci napravi jos jedan atribut za kilometrazu "mileage"
+        //napravi get i set metode za mileage
+        //prosiri jedan od konstruktora tako da moze da se koristi i setuje mileage
+        //jos vezbe definisi jos jedan atribut za stanje motora da li je ukljucen ili nije
+        
+        
+    }
 }
--- src/main/java/main/factory/Main.java
+++ src/main/java/main/factory/Main.java
@@ -18,26 +18,51 @@ public class Main {
         
         String message = "Ovo je poruka";
         
-        Car fiat = new Car();
-        fiat.model = "Fiat Punto";
-        fiat.buildYear = 2010;
-        fiat.color = "Black";
-        fiat.consumption = 6;
-        fiat.maxSpeed = 180;
-        fiat.currentFuel = 20;
-        fiat.maxFuel = 45;
-        
-        System.out.println("Model: " + fiat.model + fiat.color + fiat.buildYear + fiat.consumption);
-        
-        
-        Car ford = new Car();
-        ford.model = "Ford Fiesta";
-        ford.buildYear = 2015;
-        System.out.println("Model: " + ford.model);
-        System.out.println("Godina proizvodnje: " + ford.buildYear);
+        Car fiat = new Car("Fiat Punto", 2010, 180, "Black", 45, 20, 6);
         
         
         
+        fiat.setModel("Fiat Punto");
+        fiat.setBuildYear(2010);
+        fiat.setColor("Black");
+        fiat.setconsumption(6);
+        fiat.setCurrentFuel(20);
+        fiat.setMaxSpeed(180);
+        fiat.setMaxFuel(45);
+//        fiat.buildYear = 2010;
+//        fiat.color = "Black";
+//        fiat.consumption = 6;
+//        fiat.maxSpeed = 180;
+//        fiat.currentFuel = 20;
+//        fiat.maxFuel = 45;
+//        
+        fiat.showData();
+        
+        
+        System.out.println("________________");
+        
+        
+        Car ford = new Car("Ford", 2015, 190, "Red", 40, 10, 5);
+//        Car ford = new Car();
+//        ford.setModel("Ford Fiesta");
+//        ford.setBuildYear(2015);
+//        ford.buildYear = 2015;
+        
+        ford.showData();
+        
+        System.out.println("________________");
+        
+        
+//        System.out.println("Model: " + ford.getModel());
+        
+        Car peugeot = new Car("306", 2001, 180, "Red", 40, 7, 6);
+        
+        peugeot.showData();
+        
+        System.out.println("________________");
+        
+        Car Lada = new Car("Lada Niva", 1980, 190, "green", 40, 20, 10);
+        Lada.showData();
         
     }
     
